e4583d615f5b7a79ac32b25e5602c3ad3026855c
pensiero <ofanelli@meetup.com>
2018-07-18 14:52:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+WeWork Meetupator
+====================
+
+# Setup
+
+First time run:
+
+```bash
+./setup.sh start
+```
+
+After first setup you can use `docker-composer` to manage your containers.
+
+# Live in your browser
+
+Add the following lines in your `/etc/hosts` file.
+
+```
+127.0.0.1 www.symfony.dev
+```
---
+++ src/Entity/Meetup.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Meetup
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $sourceId;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getSourceId(): ?int
+    {
+        return $this->sourceId;
+    }
+
+    public function setSourceId(int $sourceId): self
+    {
+        $this->sourceId = $sourceId;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
